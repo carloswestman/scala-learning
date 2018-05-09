684a248d20d94023071d0463a3d42170d1290bb8
Carlos Westman <carlos@Carloss-MacBook-Pro.local>
2018-05-09 10:41:52 -0700

    initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+import scalatour.scalatour
+
+object Main extends App {
+  println("Hello, World!")
+  println("Hello again")
+  val st = new scalatour
+  st.s1_basics
+}
---
+++ src/main/scala/scalatour.scala
@@ -0,0 +1,17 @@
+package scalatour
+
+import org.scalatest._
+class scalatour{
+
+  def hello = "hello from package"
+
+  def s1_basics = {
+  	//expresions
+    println(1) // 1
+	println(1 + 1) // 2
+	println("Hello!") // Hello!
+	println("Hello," + " world!") // Hello, world!
+	assert( 1 == 2,  "expression not asserted")
+  }
+
+}
