a4d1bd9733be6229850c28e3a54135f278c5d82f
Carlos Westman <carlos@Carloss-MacBook-Pro.local>
2018-05-13 11:36:26 -0700

    fixing some ScalaStyle warnings

--- src/main/scala/scalatour.scala
+++ src/main/scala/scalatour.scala
@@ -1,28 +1,28 @@
 package scalatour
 
 import org.scalatest._
-class scalatour{
+class Scalatour{
 
-  def hello = "hello from package"
+  def hello: String = "hello from package"
 
-  def s1_basics = {
+  def s1Basics: Unit = {
 
-  	//expresions
+    //expresions
     println(1) // 1
-	  println(1 + 1) // 2
-	  println("Hello!") // Hello!
-	  println("Hello," + " world!") // Hello, world!
+    println(1 + 1) // 2
+    println("Hello!") // Hello!
+    println("Hello," + " world!") // Hello, world!
 
-	  //values and variables
-	  val x = 5
-	  val y : Int = 2
-	  // x = 3 x is inmutable, this generates a compile error
-	  var w = 4
-	  var z : Int =  5
-	  z = 6
+    //values and variables
+    val x = 5
+    val y : Int = 2
+    // x = 3 x is inmutable, this generates a compile error
+    var w = 4
+    var z : Int =  5
+    z = 6
 
-	  //blocks return the result of last expression
-	  assert({
+    //blocks return the result of last expression
+    assert({
       val x = 1 + 1
       x + 1
     } == 3)
@@ -35,9 +35,10 @@ class scalatour{
     println(add(1, 2)) // 3 
     val getTheAnswer = () => 42 // no parameter function
     println(getTheAnswer()) // 42 
-    val multilineFunction = (x: Int) => { 
-    	val y = x + 1
-    	y * x } // this is ugly but functions can be multilined
+    val multilineFunction = (x: Int) => {
+      val y = x + 1
+      y * x
+    } // this is ugly but functions can be multilined
 
     //methods
     def add2(x: Int, y: Int): Int = x + y // define output type and can be multilined
@@ -60,7 +61,59 @@ class scalatour{
     var point2 = Point(1, 2)
     assert(point == point2) // inmutable and comparable by value
 
+    //objects
+    object IdFactory {
+      private var counter = 0
+      def create(): Int = {
+        counter += 1
+        counter
+      }
+    }
+    println(IdFactory.create()) //1
+    println(IdFactory.create()) //2
+
+    //traits
+    trait Greeter3 {
+      def greet(name: String): Unit
+    }
+
+    trait Greeter4 {
+      def greet(name: String): Unit =
+        println("Hello, " + name + "!")
+    }
+
+    class DefaultGreeter extends Greeter4
+
+    class CustomizableGreeter(prefix: String, postfix: String) extends Greeter4 {
+      override def greet(name: String): Unit = {
+        println(prefix + name + postfix)
+      }
+    }
+    val greeter1 = new DefaultGreeter()
+    greeter1.greet("Scala developer") // Hello, Scala developer!
+
+    val customGreeter = new CustomizableGreeter("How are you, ", "?")
+    customGreeter.greet("Scala developer") // How are you, Scala developer?   
 
   }
+  def s2_unifiedtypes = {
+    val list: List[Any] = List(
+      "a string",
+      732,  // an integer
+      'c',  // a character
+      true, // a boolean value
+      () => "an anonymous function returning a string"
+    )
+    list.foreach(element => println(element))
 
+    val x: Long = 987654321
+    val y: Float = x  // 9.8765434E8 (note that some precision is lost in this case)
+
+    val face: Char = '☺'
+    val number: Int = face  // 9786
+
+    val a: Long = 987654321
+    val b: Float = a  // 9.8765434E8
+    //val c: Long = b  // Does not conform
+  }
 }
